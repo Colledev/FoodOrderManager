17a18fd66416565519f3d569dc0cb5dc64253658
Colledev <gabrielcolle3@hotmail.com>
2024-08-06 17:20:32 -0300

    init

---
+++ src/main/java/com/example/foodordermanager/FoodOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodordermanager/FoodOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
