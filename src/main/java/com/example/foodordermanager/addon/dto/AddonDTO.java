b2e5a416aac1e7f61339a06663b56c601f2117e3
Colledev <gabrielcolle3@hotmail.com>
2024-08-08 02:09:15 -0300

    feat: Add addonDTO to ProductAddonMapper

--- src/main/java/com/example/foodordermanager/addon/dto/AddonDTO.java
+++ src/main/java/com/example/foodordermanager/addon/dto/AddonDTO.java
@@ -6,6 +6,15 @@ public class AddonDTO {
     private String name;
     private Double price;
 
+    public AddonDTO() {
+    }
+
+    public AddonDTO(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public Long getId() {
         return id;
     }
@@ -25,7 +34,6 @@ public class AddonDTO {
     public void setName(String name) {
         this.name = name;
     }
-
 
     public void setPrice(Double price) {
         this.price = price;
